2d800d1b614d2151d665105549de72022fe987ff
adevair.junior <profadevairvitorio@gmail.com>
2025-02-12 00:04:43 -0300

    feat: exercício 3

---
+++ lista_python_basico/exercicio_3_resolvido.py
@@ -0,0 +1,12 @@
+# Peça um número inteiro e mostre o dobro dele.
+# Solicitar ao usuário um número inteiro
+# A função input() recebe o número como uma string, então usamos int() para converter a string em um número inteiro.
+numero = int(input("Digite um número inteiro: "))
+
+# Calcular o dobro do número
+# Multiplicamos o número por 2 para obter o dobro.
+dobro = numero * 2
+
+# Exibir o resultado do dobro
+# A função print() exibe a mensagem junto com o valor do dobro calculado.
+print("O dobro do número é:", dobro)
