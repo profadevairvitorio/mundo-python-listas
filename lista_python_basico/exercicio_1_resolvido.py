4bf523cdcceee9a8095cd29b45dc98a87e462cc1
adevair.junior <profadevairvitorio@gmail.com>
2025-02-12 00:02:18 -0300

    feat: exercício 1

---
+++ lista_python_basico/exercicio_1_resolvido.py
@@ -0,0 +1,13 @@
+# Peça ao usuário dois números inteiros e exiba a soma deles.
+
+# Solicitar ao usuário o primeiro número
+numero1 = int(input("Digite o primeiro número inteiro: "))
+
+# Solicitar ao usuário o segundo número
+numero2 = int(input("Digite o segundo número inteiro: "))
+
+# Calcular a soma dos dois números
+soma = numero1 + numero2
+
+# Exibir o resultado da soma
+print("A soma dos números é:", soma)
