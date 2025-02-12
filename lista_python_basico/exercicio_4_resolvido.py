b1575d4067ae688b822f0c60a47aa8e1828e85d6
adevair.junior <profadevairvitorio@gmail.com>
2025-02-12 00:05:33 -0300

    feat: exercício 4

---
+++ lista_python_basico/exercicio_4_resolvido.py
@@ -0,0 +1,11 @@
+# Solicitar ao usuário a temperatura em Celsius
+# A função input() recebe o valor como uma string, então usamos float() para converter em número decimal.
+celsius = float(input("Digite a temperatura em Celsius: "))
+
+# Calcular a temperatura em Fahrenheit
+# A fórmula para converter Celsius para Fahrenheit é: (Celsius * 9/5) + 32
+fahrenheit = (celsius * 9/5) + 32
+
+# Exibir o resultado da conversão
+# Usamos print() para exibir a temperatura convertida em Fahrenheit.
+print("A temperatura em Fahrenheit é:", fahrenheit)
