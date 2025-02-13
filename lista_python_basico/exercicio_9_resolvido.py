32aa1d2bcdefcf53afa584f77ff24265753e2eaf
adevair.junior <profadevairvitorio@gmail.com>
2025-02-13 17:48:41 -0300

    feat: resolução do exercício 9

---
+++ lista_python_basico/exercicio_9_resolvido.py
@@ -0,0 +1,10 @@
+# Peça a idade do usuário em anos e converta para dias (desconsiderando anos bissextos).
+
+# Solicitar a idade do usuário em anos
+idade_anos = int(input("Digite sua idade em anos: "))
+
+# Calcular a idade em dias (considerando 365 dias por ano)
+idade_dias = idade_anos * 365
+
+# Exibir o resultado
+print(f"Sua idade em dias é aproximadamente: {idade_dias} dias")
