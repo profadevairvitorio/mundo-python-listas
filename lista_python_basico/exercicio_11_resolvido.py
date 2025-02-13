50dc0ecb156d4ed40f7c218b6665e76d22d5cb33
adevair.junior <profadevairvitorio@gmail.com>
2025-02-13 17:55:01 -0300

    feat: resolução do exercício 11

---
+++ lista_python_basico/exercicio_11_resolvido.py
@@ -0,0 +1,7 @@
+# 11. Peça um número ao usuário e exiba sua raiz quadrada. Use a biblioteca Math
+import math
+
+num = float(input("Digite um número: "))
+
+# Calculando e exibindo a raiz quadrada
+print("Raiz quadrada:", math.sqrt(num))
