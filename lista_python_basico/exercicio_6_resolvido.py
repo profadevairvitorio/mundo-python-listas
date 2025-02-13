06fea37d136635e8be8e53e9d832d8f942b35ff6
adevair.junior <profadevairvitorio@gmail.com>
2025-02-13 17:45:47 -0300

    feat: resolução do exercício 6

---
+++ lista_python_basico/exercicio_6_resolvido.py
@@ -0,0 +1,8 @@
+# Pedir um número ao usuário
+numero = float(input("Digite um número: "))
+
+# Calcula o quadrado do número
+quadrado = numero ** 2
+
+# Resultado
+print(f"O quadrado de {numero} é: {quadrado}")
