4eaf8f248774eb9804cd6c01c1f3fd39fb3eaec9
adevair.junior <profadevairvitorio@gmail.com>
2025-02-13 22:46:01 -0300

    feat: resolução do exercício 13

---
+++ lista_python_basico/exercicio_13_resolvido.py
@@ -0,0 +1,7 @@
+# 13. Peça um número inteiro e exiba o fatorial dele. Use a biblioteca Math
+import math
+
+num = int(input("Digite um número: "))
+
+# Calculando e exibindo o fatorial
+print("Fatorial:", math.factorial(num))
