990da23ea3f5ec5f5439992aefec9a579372eeaf
adevair.junior <profadevairvitorio@gmail.com>
2025-02-13 23:05:53 -0300

    feat: resolução do exercício 26

---
+++ lista_python_basico/exercicio_26_resolvido.py
@@ -0,0 +1,8 @@
+# 26. Sua empresa fará um sorteio entre 10 participantes numerados de 1 a 10.
+# Gere um número aleatório para definir o vencedor.
+import random
+
+vencedor = random.randint(1, 10)
+
+# Exibindo o número do vencedor
+print(f"O vencedor é o participante número {vencedor}.")
